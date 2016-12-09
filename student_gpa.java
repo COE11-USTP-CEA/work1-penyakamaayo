55ce1d5b6f5f043bdd762558b69b64815c755db0
jetclitz <jetclits@yahoo.com>
2016-12-09 14:55:35 +0800

    changed student name and grades

--- student_gpa.java
+++ student_gpa.java
@@ -3,26 +3,26 @@ import java.util.List;
 class student_gpa{
 	public static void main(String[] args){
 		/*
-		student gabs = new student();
-		gabs.setName("Benjo Gaburno");
-		gabs.setDept("CEA");
+		student penya = new student();
+		penya.setName("Benjo Gaburno");
+		penya.setDept("CEA");
 
 		subject oop_class = new subject();
 		oop_class.setSubjectName("Object Oriented Programming");
 		oop_class.setGrade(75.00f);
-		gabs.addSubject(oop_class);
+		penya.addSubject(oop_class);
 
-		subject trig_class = new subject();
-		trig_class.setSubjectName("Trigonometry");
-		trig_class.setGrade(61.17f);
-		gabs.addSubject(trig_class);
+		subject intgc_class = new subject();
+		intgc_class.setSubjectName("intgconometry");
+		intgc_class.setGrade(61.17f);
+		penya.addSubject(intgc_class);
 
-		System.out.println("Student is " + gabs.getName());
-		System.out.println("Dept: " + gabs.getDept());
+		System.out.println("Student is " + penya.getName());
+		System.out.println("Dept: " + penya.getDept());
 		
 		System.out.println("Subjects of Student");
 		System.out.println("-------------------");
-		List<subject> subjects_ni_abs = gabs.getSubjects();
+		List<subject> subjects_ni_abs = penya.getSubjects();
 		
 		for(subject s : subjects_ni_abs){
 			System.out.println(" -- Subject: " + s.getSubjectName());
@@ -31,28 +31,28 @@ class student_gpa{
 		*/
 		
 		
-		student gabs = new student();
-		gabs.setName("Benjo Gaburno");
-		gabs.setDept("CEA");
+		student penya = new student();
+		penya.setName("Stephen Peña");
+		penya.setDept("CEA");
 
 		subject oop_class = new subject();
 		oop_class.setSubjectName("Object Oriented Programming");
-		oop_class.setGrade(75.00f);
-		gabs.addSubject(oop_class);
+		oop_class.setGrade(89.00f);
+		penya.addSubject(oop_class);
 
-		subject trig_class = new subject();
-		trig_class.setSubjectName("Trigonometry");
-		trig_class.setGrade(61.17f);
-		gabs.addSubject(trig_class);
+		subject intgc_class = new subject();
+		intgc_class.setSubjectName("Integral Calculus");
+		intgc_class.setGrade(88.42f);
+		penya.addSubject(intgc_class);
 
-		System.out.println("Student is " + gabs.getName());
-		System.out.println("Dept: " + gabs.getDept());
+		System.out.println("Student is " + penya.getName());
+		System.out.println("Dept: " + penya.getDept());
 		
 		System.out.println("Subjects of Student");
 		System.out.println("-------------------");
-		List<subject> subjects_ni_abs = gabs.getSubjects();
+		List<subject> subjects_ni_penya = penya.getSubjects();
 		
-		for(subject s : subjects_ni_abs){
+		for(subject s : subjects_ni_penya){
 			System.out.println(" -- Subject: " + s.getSubjectName());
 			System.out.println("    Grade is " + s.getGrade());
 		}
